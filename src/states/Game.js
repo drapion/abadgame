fea745ff9c4841f11beb125d31f043d60131ada0
Jack Thompson <jacktinternetboy@gmail.com>
2017-03-11 16:17:12 -0500

    tweaked motion

--- src/states/Game.js
+++ src/states/Game.js
@@ -2,7 +2,7 @@ import Phaser from 'phaser';
 
 export default class extends Phaser.State {
   init() {
-    var cursors
+    this.jumpTimer = 0;
   }
 
   preload() {
@@ -16,31 +16,36 @@ export default class extends Phaser.State {
     this.map.addTilesetImage('grass');
 
     this.layer = this.map.createLayer('Tile Layer 1');
-    this.map.setCollisionBetween(1, 100, true, 'Tile Layer 1');
+
+    this.map.setCollisionBetween(1, 2);
 
     this.dan = this.add.sprite(0, 0, 'dan');
     this.physics.enable(this.dan, Phaser.Physics.ARCADE);
     this.dan.body.collideWorldBounds = true;
-    this.dan.body.gravity.set(0, 180);
+    this.dan.body.gravity.set(0, 450);
 
-    this.cursors = this.game.input.keyboard.createCursorKeys();
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.camera.follow(this.dan);
+    this.camera.deadzone = new Phaser.Rectangle(100, 100, 600, 400);
   }
 
   update() {
+    this.physics.arcade.collide(this.dan, this.layer);
 
-    game.physics.arcade.collide(this.dan, this.layer);
     this.dan.body.velocity.x = 0;
 
-    if (this.cursors.up.isDown && this.dan.body.onFloor()) {
-      this.dan.body.velocity.y = -150;
-    } else if (this.cursors.right.isDown) {
-      console.log("right down");
-      this.dan.body.velocity.x = 150;
-    } else if (this.cursors.left.isDown) {
-      console.log("left down");
+    if (this.cursors.left.isDown) {
       this.dan.body.velocity.x = -150;
     }
+    if (this.cursors.right.isDown) {
+      this.dan.body.velocity.x = 150;
+    }
 
+    if (this.cursors.up.isDown && this.dan.body.onFloor() && this.time.now > this.jumpTimer) {
+        this.dan.body.velocity.y = -250;
+        this.jumpTimer = this.time.now + 750;
+    }
   }
 
   render() {
