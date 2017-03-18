cddfc0065a9754f007201b18b4c9c9e493b1af8e
Meh Man <Meh Man>
2017-03-18 14:38:20 -0400

    oops

--- src/states/preloader.js
+++ src/states/preloader.js
@@ -17,14 +17,11 @@ export default class extends Phaser.State {
     this.load.image('bush', 'assets/tileset/bush.png');
     this.load.tilemap('intromap', 'assets/tilemap/intro.json', null, Phaser.Tilemap.TILED_JSON);
     this.load.tilemap('map', 'assets/tilemap/map.json', null, Phaser.Tilemap.TILED_JSON);
-<<<<<<< HEAD
     this.load.spritesheet('dan', 'assets/general/dan.png', 25, 32);
     this.load.image('alex', 'assets/general/alextest.png');
     this.load.audio('laugh', 'assets/intro/alexlaugh.mp3');
-=======
     this.load.spritesheet('dan', 'assets/general/danhuman.png', 14, 32);
     this.load.spritesheet('bullet', 'assets/general/bullet.png', 5, 4);
->>>>>>> 9fa21b56365d4713965d4723e71e93de4f3352c9
   }
 
   create() {
