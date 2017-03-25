e561b863fe95dda2efee7d794fee6f337ac8c252
drapion <21jban_39@perrysburgstudents.net>
2017-03-25 12:22:41 -0400

    k

--- src/states/Game.js
+++ src/states/Game.js
@@ -1,54 +1,19 @@
 import Phaser from 'phaser';
 
 export default class extends Phaser.State {
-  init() {
-    this.jumpTimer = 0;
-  }
-
-  preload() {
+  init () {
 
   }
 
-  create() {
-    this.physics.startSystem(Phaser.Physics.ARCADE);
+  preload () {
 
-    this.map = this.add.tilemap('map');
-    this.map.addTilesetImage('grass');
-
-    this.layer = this.map.createLayer('Tile Layer 1');
-
-    this.map.setCollisionBetween(1, 2);
-
-    this.dan = this.add.sprite(0, 0, 'dan');
-    this.physics.enable(this.dan, Phaser.Physics.ARCADE);
-    this.dan.body.collideWorldBounds = true;
-    this.dan.body.gravity.set(0, 450);
-
-    this.cursors = this.input.keyboard.createCursorKeys();
-
-    this.camera.follow(this.dan);
-    this.camera.deadzone = new Phaser.Rectangle(100, 100, 600, 400);
   }
 
-  update() {
-    this.physics.arcade.collide(this.dan, this.layer);
+  create () {
 
-    this.dan.body.velocity.x = 0;
-
-    if (this.cursors.left.isDown) {
-      this.dan.body.velocity.x = -150;
-    }
-    if (this.cursors.right.isDown) {
-      this.dan.body.velocity.x = 150;
-    }
-
-    if (this.cursors.up.isDown && this.dan.body.onFloor() && this.time.now > this.jumpTimer) {
-        this.dan.body.velocity.y = -250;
-        this.jumpTimer = this.time.now + 750;
-    }
   }
 
-  render() {
+  render () {
 
   }
 }
