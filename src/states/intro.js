b55de74bb630ce7a4339a4f787a17bb1ab362995
Meh Man <Meh Man>
2017-03-25 12:34:57 -0400

    that's hot

--- src/states/intro.js
+++ src/states/intro.js
@@ -41,6 +41,14 @@ export default class extends Phaser.State {
       this.alexmove = this.add.tween(this.alex);
       this.alexmove.to({ x: 340 }, 100, 'Linear', true, 0);
     }, this);
+
+    this.time.events.add(Phaser.Timer.SECOND * 9.5, () => {
+      this.alexmove = this.add.tween(this.alex);
+      this.alexmove.to({ x: -100 }, 500, 'Linear', true, 0);
+      this.guy.kill();
+    }, this);
+
+    this.input.onDown.add(this.skip, this);
   }
 
   update() {
@@ -50,78 +58,8 @@ export default class extends Phaser.State {
   render() {
 
   }
+
+  skip () {
+    this.game.state.start('Game');
+  }
 }
-
-/*
-this.world.setBounds(0, 0, 800, 600);
-        this.stage.backgroundColor = '#FFF8FF';
-        this.music = this.add.audio('intromus');
-        this.music.play();
-        this.music.volume = 0;
-
-        this.time.events.add(Phaser.Timer.SECOND * 4.46, () => {
-            this.fbi = this.add.image(-125, this.world.centerY - 175, 'FBI');
-            this.fbi.scale.x = .5;
-            this.fbi.scale.y = .5;
-            this.jump = this.fbi.animations.add('jump', [0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1, 0], 13, true);
-            this.jump2 = this.fbi.animations.add('jump2', [0, 1, 2, 3, 4, 5, 6], 13, false);
-            this.jump3 = this.fbi.animations.add('jump3', [6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6], 13, false);
-            this.hop = this.fbi.animations.add('hop', [7, 8, 9, 10], 10, false);
-
-            this.trumpback = this.add.image(816, this.world.centerY - 200, 'trumpback');
-            this.trumpback.scale.x = .6;
-            this.trumpback.scale.y = .6;
-            this.trumpattack = this.trumpback.animations.add('attack', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 10, 11, 12, 13], 9, false);
-
-            this.fbitween = this.add.tween(this.fbi);
-
-            this.fbitween.to({ x: 470 }, 1200, 'Linear', true, 0);
-
-            this.trumptween = this.add.tween(this.trumpback);
-
-            this.trumptween.to({ x: 110 }, 1200, 'Linear', true, 0);
-        }, this);
-
-        this.bartop = this.add.sprite(0, 0, 'blackbar');
-        this.bartop.scale.x = 2;
-        this.barbottom = this.add.sprite(0, 400, 'blackbar');
-        this.barbottom.scale.x = 2;
-        this.barbottom.scale.y = 2.5;
-
-        this.time.events.add(Phaser.Timer.SECOND * 5.674230, () => {
-            this.jump.play();
-            this.jump.onLoop.add(() => {
-                if (this.jump.loopCount == 1) {
-                        this.jump.loop = false;
-                    }
-            }, this);
-        }, this);
-
-        this.time.events.add(Phaser.Timer.SECOND * 7.97, () => {
-            this.trumpattack.play();
-        }, this);
-
-        this.time.events.add(Phaser.Timer.SECOND * 8.94, () => {
-            this.jump2.play();
-        }, this);
-
-        this.time.events.add(Phaser.Timer.SECOND * 10.965, () => {
-            this.jump3.play();
-        }, this);
-
-        this.time.events.add(Phaser.Timer.SECOND * 12.465, () => {
-            this.hop.play();
-        }, this);
-
-        this.time.events.add(Phaser.Timer.SECOND * 15, () => {
-            this.blackscreen = this.add.sprite(0, 0, 'blackbar');
-            this.blackscreen.scale.x = 10;
-            this.blackscreen.scale.y = 10;
-        }, this);
-
-        this.time.events.add(Phaser.Timer.SECOND * 15.5, () => {
-            this.game.state.start('Map');
-        }, this);
-
-        this.input.onDown.add(this.skip, this);
-*/
